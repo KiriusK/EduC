4e2247f07e136e3b5d3f785a957c10746b68d62d
Kirius <kvarton@rambler.ru>
2024-03-23 21:27:41 +0300

    Started lection 3

---
+++ lec3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task 1
+double Calculate(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    double denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+double result = Calculate(2, 3, 4, 5);
+Console.WriteLine(result);
+
+// Task 2 22:38
